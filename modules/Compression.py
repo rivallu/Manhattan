28dc1dc74c8068b95e18e40634ceb99d7834d0a3
Lucas Rival <lucas.rival@gmail.com>
2017-05-24 14:28:26 +0200

    added the compression function

--- modules/Compression.py
+++ modules/Compression.py
@@ -3,4 +3,10 @@
 
 
 class Compression:
-    
+
+    def compress(String):
+        """
+            Fonction qui permet de compresser une chaine de caractère en
+            suivant l'algo Lempel-Ziv-Welch.
+            :param: une chaine de caractère
+        """
