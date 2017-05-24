89eae098ba03591f00adaaf46a8a9e5d5ddeb0df
Lucas Rival <lucas.rival@gmail.com>
2017-05-24 14:41:10 +0200

    added the base algo

--- modules/Compression.py
+++ modules/Compression.py
@@ -9,4 +9,18 @@ class Compression:
             Fonction qui permet de compresser une chaine de caractère en
             suivant l'algo Lempel-Ziv-Welch.
             :param: une chaine de caractère
+            :return: la chaine compressé
         """
+        output=
+        dico={}
+        w=null
+        for letter in String:
+            element='{}{}'.format(w,letter)
+            if dico.has_key(element):
+                w=element
+            else:
+                dico.update(element:??)
+                output+=w
+                w=letter;
+        print(output)
+        return output
