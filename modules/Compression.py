88e9c1a5e3aec47216dd18f00b7dfb0fac9c5507
Lucas Rival <lucas.rival@gmail.com>
2017-05-30 10:42:33 +0200

    added functions

--- modules/Compression.py
+++ modules/Compression.py
@@ -2,25 +2,36 @@
 #-*-coding:UTF-8-*-
 
 
-class Compression:
+def genDict():
+    """
+    Fonction qui génère un dictionnaire
+    :return: le dictionnaire
+    """
+    dico={}
+    for i in range(0,256):
+        dico.update({chr(i):i})
+    return dico
 
-    def compress(String):
-        """
-            Fonction qui permet de compresser une chaine de caractère en
-            suivant l'algo Lempel-Ziv-Welch.
-            :param: une chaine de caractère
-            :return: la chaine compressé
-        """
-        output=
-        dico={}
-        w=null
-        for letter in String:
-            element='{}{}'.format(w,letter)
-            if dico.has_key(element):
-                w=element
-            else:
-                dico.update(element:??)
-                output+=w
-                w=letter;
-        print(output)
-        return output
+def compress(String):
+    """
+        Fonction qui permet de compresser une chaine de caractère en
+        suivant l'algo Lempel-Ziv-Welch.
+        :param: une chaine de caractère
+        :return: la chaine compressé
+    """
+    output=b''
+    dico=genDict()
+    w=''
+    for letter in String:
+        element='{}{}'.format(w,letter)
+        if element in dico:
+            w=element
+        else:
+            dico.update({element:len(dico)})
+            output+=str.encode(bin(dico.get(w)))
+            w=letter;
+    return output
+
+
+if __name__ == '__main__':
+    print(compress("TOBEORNOTTOBEORTOBEORNOT"))
