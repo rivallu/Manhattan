7cc7f6a91a94923e58e0d1c405aa1f85eece84cd
Lucas Rival <lucas.rival@gmail.com>
2017-05-31 09:56:19 +0200

    added generic Compress method and comment

--- modules/Compression.py
+++ modules/Compression.py
@@ -12,13 +12,28 @@ def genDict():
         dico.update({chr(i):i})
     return dico
 
-def compress(String):
+def compress(fileIn,fileOut,algo):
+    """
+    Fonction générique qui fait les appels au différentes fonctions pour compresser
+    en fonction de l'algo donnée
+    :param fileIn: Le fichier à compresser
+    :param fileOut: le fichier de sortie
+    :param algo: l'algo utilisé pour la compression.
+    """
+    if algo=="lzw":
+        datas=openFileToCompress(fileIn)
+        writteFile(compressLZW(datas),fileOut)
+    else:
+        print('Error')
+
+def compressLZW(String):
     """
     Fonction qui permet de compresser une chaine de caractère en
     suivant l'algo Lempel-Ziv-Welch.
     :param: une chaine de caractère
     :return: la chaine compressé
     """
+    print(String)
     output=''
     dico=genDict()
     w=''
@@ -29,6 +44,7 @@ def compress(String):
         else:
             dico.update({element:len(dico)})
             binary="{0:b}".format(dico.get(w))
+            # print('{} {}'.format(letter,binary))
             binary=paddin(binary)
             output+=binary
             w=letter;
@@ -45,27 +61,47 @@ def paddin(binary):
         binary="{0:b}".format(0)+binary
     return binary
 
-def writteFile(bytesString):
+def writteFile(bytesString,fileName):
     """
     Fonction qui permet d'écrire le contenu compresser dans un fichier binaire.
     :param: la chaine compresser en binaire
     :raise: IOEerror retourne le code d'erreur de la classe IO.
     """
     try:
-        with open('archive.lzw','wb+') as archive:
+        with open(fileName,'wb+') as archive:
             for h in range(0,len(bytesString),8):
                 hexString=bytesString[h:h+8]
                 hexInt=int(hexString,2)
                 archive.write(bytes([hexInt]))
             archive.close()
-    except IOError as e:
-        print('Error')
+    except IOError as error:
+        print('writteFile')
+        raise error
+
+def openFileToCompress(fileName):
+    """
+    Fonctio qui va lire le fichier passé en paramètre et va retoruner tout son
+    contenu.
+
+    :param: le nom du fichier à ouvrir
+    :raise: IOError, les erreurs éventuelle sur la lecture du fichier.
+    """
+    try:
+        with open(fileName,'r') as f:
+            lines=f.readlines()
+            lines=" ".join(lines)
+            f.close()
+            return lines
+    except IOError as error:
+        print('openFileToCompress')
+        raise error
 
 if __name__ == '__main__':
     # assert(paddin('000001')=='0000000001')
     # assert(paddin('000001')!='00000001')
-    f= open("test",'r')
-    line=f.read()
-    f.close
+    # f= open("../test/test",'r')
+    # line=f.read()
+    # f.close
     # print(compress("TOBEORNOTTOBEORTOBEORNOT"))
-    writteFile(compress(line))
+    print(compress("TOBEORNOTTOBEORTOBEORNOT"))
+    # writteFile(compress(line))
