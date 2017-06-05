fc80a20db2f9eb3e43ff74a211e33ce51c75aef5
Aegiiis <alexandremattheydoret@gmail.com>
2017-06-05 14:37:10 +0200

    Enrichissement de la documentation

--- modules/Decompression.py
+++ modules/Decompression.py
@@ -7,10 +7,10 @@ def CinDico(caractere, dictionnaire):
     """
     Fonction permettant de détecter la présence d'un code dans le dictionnaire construit par Decompress.
     
-    Elle retourne -1 si le code ne s'y trouve pas. Elle retourne la position du code dans la liste en cas de détection. Cela permet à Decompress de récupérer la suite de caractères correspondante.
-    
     :param caractere: Paramètre correspondant au code d'un caractère ou d'une suite de caractères compressé.
     :param dictionnaire: Liste correspondant au dictionnaire en cours d'utilisation et construit par Decompress()
+    
+    :return test: Variable de type Integer ayant pour valeur -1 si le code ne se trouve pas dans le dictionnaire entrée en paramètre ou la position du code dans le dictionnaire en cas de détection. Cela permet à Decompress de récupérer la suite de caractères correspondante.
     """
     
     test = -1
@@ -45,6 +45,10 @@ def LectureFichier(chemin):
     Enfin, la fonction retourne la liste correctement formatée et exploitable par Decompress()
     
     :param chemin: Paramètre contenant le chemin d'accès de l'archive à décompresser.
+    
+    :raise IndexError: Afin de coder les 9ème bits en hexadecimal, il a fallu ajouter des 0 non significatifs à la fin de la liste des 9ème bits. En effet, nous devions posséder une liste de 9ème bits divisible par 8 pour coder en hexadécimal d'où l'ajout de zéro. Cela entraîne ici une erreur IndexError étant donné qu'il n'existe pas de code en hexadécimal pour les 9ème bits ajoutés artificiellement. On sort alors de la boucle for en provoquant un break une fois cette erreur rencontrée (qui correspondra donc au premier 9ème bits non significatifs).
+    
+    :return ligne: Liste formatée des codes de caractères sur 9 bits prête à être interprêtée par Decompress()
     """
     
     fichier = open(chemin, 'rb')
@@ -104,6 +108,8 @@ def Decompress(caracteres,fileOut):
     
     :param caracteres: Il s'agit d'une liste dont chaque élément correspond au code ASCII ou du dictionnaire d'un caractère ou d'une suite de caractère compressé. Ce code doit être exprimé en bit, type string sans le 0b ajouté par le type bytes. LectureFichier() peut fournir une telle liste formatée à partir d'un fichier texte.
     :param fileOut: Paramètre contenant le chemin d'accès souhaité par l'utilisateur pour écriture du fichier décompressé.
+    
+    :raise IndexError: Lors du premier ajout au dictionnaire, la liste est vide. Or, dans le try, nous essayons de récupérer le dernier indice du code ajouté au dictionnaire. Cela générère une erreur IndexError qui est gérée dans le except. En effet, dans ce cas, nous ajoutons au dictionnaire le code avec pour indice 256 ce qui permet de l'initialiser et de ne plus rencontrer l'erreur par la suite.
     """
     
     resultat = open(fileOut, 'w')
