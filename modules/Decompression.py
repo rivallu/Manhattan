688764ac5afe7dc89451766ab1b1a6bdf73f785d
Aegiiis <alexandremattheydoret@gmail.com>
2017-06-05 13:02:23 +0200

    Décompression accepte le nouveau format de fichier

--- modules/Decompression.py
+++ modules/Decompression.py
@@ -23,41 +23,53 @@ def LectureFichier(chemin):
     fichier = open(chemin, 'rb')
 
     ligne = fichier.readlines()
-    
+    print(ligne)
     binary = ""
-    print(ligne)
-    print("\n")
     n = len(ligne)
-    for i in range(1, n-1):
+    ninebit = ""
+    sauv = 0
+    for i in range(0, n):
+        if ligne[i] == b'\n' :
+            sauv = i
+            
+    for i in range(1, sauv):
         ligne[0] = ligne[0] + ligne[i]
+        
+    for i in range(sauv+1, n):
+        ligne[sauv] = ligne[sauv] + ligne[i]
+        
+    ligne = [ligne[0], ligne[sauv][1:]]
     print(ligne)
     print("\n")
-    ligne = [ligne[0], ligne[-1]]
-    print(ligne) 
-    print("\n")
-    
     for i in range(0, len(ligne[1])):
         bit9 = str(bin(ligne[1][i]))
-        n = len(bit9)-2
+        n = len(bit9) - 2
         
         zero = ""
         for k in range(0, 8-n):
             zero = zero + "0"
         bit9 = zero + bit9[2:]
         
+        ninebit = ninebit + bit9
+        
         for j in range(0, len(bit9)) :
-            carac = str(bin(ligne[0][8*i + j]))
+            try :
+                carac = str(bin(ligne[0][8*i + j]))
+            except IndexError :
+                break
+    
             n = len(carac) - 2
             zero = ""
             for k in range(0, 8-n):
                 zero = zero + "0"
             carac = "0b" + zero + carac[2:]
             binary = binary + "0b" + bit9[j] + carac[2:] + " "
+            print(binary)
 
     ligne = binary.split("0b")
     ligne.remove('')
     print(ligne)
-    print("\n")
+    print("\n", ninebit)
     fichier.close()
     return ligne
 
@@ -79,18 +91,19 @@ def Decompress(caracteres,fileOut):
 
         elif test_code > 255 and presence == -1 :
             entree = w + w[0]
+            
         else :
             entree = chr(test_code)
 
         resultat.write(entree)
-        
-        if len(dictionnaire) == 256 :
-            dictionnaire = []
             
         try :
             dictionnaire.append([dictionnaire[-1][0] + 1, w + entree[0]])
         except IndexError :
             dictionnaire.append([256, w + entree[0]])
+            
+        if len(dictionnaire) == 256 :
+            dictionnaire = []
         
         w = entree
 
